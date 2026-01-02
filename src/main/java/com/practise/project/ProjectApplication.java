f824154a18ced73eb642bd7876f3fbf28f4764e6
shivam <shivam_sah@cms.co.in>
2026-01-03 02:59:46 +0530

    enabled Authlibrary

--- src/main/java/com/practise/project/ProjectApplication.java
+++ src/main/java/com/practise/project/ProjectApplication.java
@@ -1,5 +1,6 @@
 package com.practise.project;
 
+import com.authlibrary.configuration.EnableAuthLib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAuthLib
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ProjectApplication {
 
